03b629175ddaef2d8247df99930b410d507326e0
민경호 <1736s@naver.com>
2023-03-03 15:57:22 +0900

    style: init Country

---
+++ src/components/Molecules/Country/Country.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Country from "./Country";
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+const meta: Meta<typeof Country> = {
+  title: "Molecules/Country",
+  component: Country,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Country>;
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
+export const Primary: Story = {
+  args: {},
+};
---
+++ src/components/Molecules/Country/Country.tsx
@@ -0,0 +1,7 @@
+import * as S from "./Country.style";
+
+const Country = () => {
+  return <S.Country></S.Country>;
+};
+
+export default Country;
