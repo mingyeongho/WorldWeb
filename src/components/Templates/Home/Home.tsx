9eb5e87ac60ffe741bb43a7b3e0bc7467408fa24
민경호 <1736s@naver.com>
2023-03-03 16:28:42 +0900

    feat: add HomeTemplate child

--- src/components/Templates/Home/Home.tsx
+++ src/components/Templates/Home/Home.tsx
@@ -1,7 +1,14 @@
+import Form from "../../Molecules/Form/Form";
+import CountriesTab from "../../Organisms/CountriesTab/CountriesTab";
 import * as S from "./Home.style";
 
 const Home = () => {
-  return <S.Home></S.Home>;
+  return (
+    <S.Home>
+      <Form />
+      <CountriesTab />
+    </S.Home>
+  );
 };
 
 export default Home;
