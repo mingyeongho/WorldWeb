1d76c85fd8a53c59c2adc7af0564c3a30bb45179
민경호 <1736s@naver.com>
2023-03-03 15:49:01 +0900

    chore: install styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { GlobalStyle } from "../styles/GlobalStyle";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>세계웹</title>
       </Head>
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   );
